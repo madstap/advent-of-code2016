946f4f33f4e3f54062ce639d6cf691b0647ea2bd
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-10-11 13:55:30 -0300

    Let there be light

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of code 2016
+
+My solutions to these [puzzles](http://adventofcode.com/2016/).
+
+## License
+
+This source code is in the public domain.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject advent2016 "0.1.0-SNAPSHOT"
+  :description "Advent of code from 2016"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [madstap/comfy "1.0.4"]
+                 [medley "1.0.0"]])
