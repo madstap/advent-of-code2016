44d45f9cf54771577e490e8b8c540eeb9f44ae05
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-10-11 21:31:31 -0300

    day3 part2

--- src/advent2016/day3.clj
+++ src/advent2016/day3.clj
@@ -28,3 +28,27 @@
   (solve-part1 input) ;=> 1032
 
   )
+
+(defn re-order-vertically [triangles]
+  (->> triangles (apply mapcat vector) (partition 3)))
+
+(defn solve-part2 [input]
+  (solve-part1 (re-order-vertically input)))
+
+(comment
+
+  (def ordering-example
+    (parse
+     "101 301 501
+      102 302 502
+      103 303 503
+      201 401 601
+      202 402 602
+      203 403 603"))
+
+  (= (re-order-vertically ordering-example)
+     (map #(take 3 (iterate inc %)) (range 101 602 100)))
+
+  (solve-part2 input) ;=> 1838
+
+  )
