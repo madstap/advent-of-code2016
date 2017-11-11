c191428e7033bf392eeb340f9af0d29cefd61270
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-11-11 02:19:30 -0300

    Solve day5 part2

--- src/advent2016/day5.clj
+++ src/advent2016/day5.clj
@@ -1,7 +1,7 @@
 (ns advent2016.day5
   (:require
    [clojure.string :as str]
-   [medley.core :as medley])
+   [madstap.comfy :as comfy])
   (:import
    (java.security MessageDigest)
    (java.math BigInteger)))
@@ -31,5 +31,36 @@
   (= "18f47a30" (time (solve-part1 "abc")))
 
   (time (solve-part1 input)) ;=> "c6697b55"
+
+  )
+
+(defn render [m]
+  (apply str (map #(get m % "_")  (range 8))))
+
+(defn solve-part2 [inp]
+  (reduce (fn [acc n]
+            (let [hash (md5 (str inp n))
+                  next-entry
+                  (when (str/starts-with? hash "00000")
+                    (let [pos (-> hash (nth 5) (str) (comfy/str->int))]
+                      (when (and (contains? (set (range 8)) pos)
+                                 (not (contains? acc pos)))
+                        [pos (nth hash 6)])))]
+              (if next-entry
+                (let [acc' (conj acc next-entry)
+                      password (render acc')]
+                  (println password)
+                  (if (= (set (keys acc')) (set (range 8)))
+                    (reduced password)
+                    acc'))
+                acc)))
+          {}
+          (range)))
+
+(comment
+
+  (= "05ace8e3" (time (solve-part2 "abc")))
+
+  (time (solve-part2 input))
 
   )
